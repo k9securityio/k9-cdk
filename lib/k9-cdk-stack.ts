7da34144a6a43634000c0b975c24febdec6383e4
Stephen Kuenzli <skuenzli@k9security.io>
2020-10-02 10:40:18 -0700

    Rough out core types for k9-cdk constructs:
    * K9BucketPolicy
    * K9BucketPolicyProps
    
    Use these types in a extremely minimal test.

--- lib/k9-cdk-stack.ts
+++ lib/k9-cdk-stack.ts
@@ -1,9 +1,53 @@
 import * as cdk from '@aws-cdk/core';
+import {BucketPolicy, BucketPolicyProps} from "@aws-cdk/aws-s3";
+
+export type ArnEqualsTest = {
+    value: "ArnEquals";
+};
+
+export type ArnLikeTest = {
+    value: "ArnLike";
+};
+
+export type ArnConditionTest =
+    | ArnEqualsTest
+    | ArnLikeTest;
+
+export class K9AccessCapabilities {
+
+    constructor(
+        readonly allowAdministerResourceArns?: Set<string>,
+        readonly allowAdministerResourceTest?: ArnConditionTest,
+        readonly allowReadDataArns?: Set<string>,
+        readonly allowReadDataTest?: ArnConditionTest,
+        readonly allowWriteDataArns?: Set<string>,
+        readonly allowWriteDataTest?: ArnConditionTest,
+        readonly allowDeleteDataArns?: Set<string>,
+        readonly allowDeleteDataTest?: ArnConditionTest,
+    ) {
+
+    }
+
+    // ??? Add support for custom actions
+    // will probably encourage users to create custom statements directly using the policy instead of trying to model
+}
+
+export interface K9BucketPolicyProps extends BucketPolicyProps {
+    readonly k9AccessCapabilities: K9AccessCapabilities
+}
+
+
+export class K9BucketPolicy extends BucketPolicy {
+    constructor(scope: cdk.Construct, id: string, props: K9BucketPolicyProps) {
+        super(scope, id, props);
+    }
+}
 
 export class K9CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    // TODO - Remove stack definition.  We're not going to vend a stack.
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // The code that defines your stack goes here
+    }
 }
--- test/k9-cdk.test.ts
+++ test/k9-cdk.test.ts
@@ -1,13 +1,48 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {expect as expectCDK, haveResource} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as K9Cdk from '../lib/k9-cdk-stack';
+import {K9AccessCapabilities, K9BucketPolicyProps} from '../lib/k9-cdk-stack';
+import {Bucket} from "@aws-cdk/aws-s3/lib/bucket";
 
-test('Empty Stack', () => {
+test('k9 bucket policy', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new K9Cdk.K9CdkStack(app, 'MyTestStack');
+    const stack = new cdk.Stack(app, 'K9BucketPolicyTest');
+    const bucket = new Bucket(stack, 'TestBucket');
+
+    const administerResourceArns = new Set<string>([
+            "arn:aws:iam::139710491120:user/ci",
+        ]
+    );
+
+    const writeDataArns = new Set<string>([
+            "arn:aws:iam::12345678910:role/app-backend",
+        ]
+    );
+    const readDataArns = new Set<string>(writeDataArns)
+        .add("arn:aws:iam::12345678910:role/customer-service");
+
+    const k9AccessCapabilities: K9AccessCapabilities = {
+        allowAdministerResourceArns: administerResourceArns,
+        allowWriteDataArns: writeDataArns,
+        allowReadDataArns: readDataArns,
+        // omit delete data ARNs and 'test' configs -- use ArnEquals
+    };
+
+    const k9BucketPolicyProps: K9BucketPolicyProps = {
+        bucket: bucket,
+        k9AccessCapabilities: k9AccessCapabilities
+    };
+
+
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(k9BucketPolicyProps.k9AccessCapabilities).toEqual(k9AccessCapabilities);
+
+    // Unit Test: k9 access capabilities
+    expect(k9AccessCapabilities.allowAdministerResourceArns).toEqual(administerResourceArns);
+    // expect(k9AccessCapabilities.allowAdministerResourceTest).toBe(ArnEqualsTest)
+    expect(k9AccessCapabilities.allowWriteDataArns).toEqual(writeDataArns);
+    expect(k9AccessCapabilities.allowReadDataArns).toEqual(readDataArns);
+    expect(k9AccessCapabilities.allowDeleteDataArns).toBeUndefined();
+
+    expectCDK(stack).to(haveResource("AWS::S3::Bucket"))
 });
