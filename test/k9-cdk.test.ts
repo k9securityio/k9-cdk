12a1fb9955b5971a50208fe0b666083c2c713232
Stephen Kuenzli <skuenzli@k9security.io>
2020-10-02 12:45:16 -0700

    Extract unit test for K9AccessCapabilities

--- test/k9-cdk.test.ts
+++ test/k9-cdk.test.ts
@@ -3,23 +3,48 @@ import * as cdk from '@aws-cdk/core';
 import {K9AccessCapabilities, K9BucketPolicyProps} from '../lib/k9-cdk-stack';
 import {Bucket} from "@aws-cdk/aws-s3/lib/bucket";
 
-test('k9 bucket policy', () => {
+const administerResourceArns = new Set<string>([
+        "arn:aws:iam::139710491120:user/ci",
+    ]
+);
+
+const writeDataArns = new Set<string>([
+        "arn:aws:iam::12345678910:role/app-backend",
+    ]
+);
+const readDataArns = new Set<string>(writeDataArns)
+    .add("arn:aws:iam::12345678910:role/customer-service");
+
+const deleteDataArns = new Set<string>([
+        "arn:aws:iam::139710491120:user/super-admin",
+    ]
+);
+
+test('K9AccessCapabilities', () => {
+
+    const k9AccessCapabilities: K9AccessCapabilities = {
+        allowAdministerResourceArns: administerResourceArns,
+        allowWriteDataArns: writeDataArns,
+        allowReadDataArns: readDataArns,
+        allowDeleteDataArns: deleteDataArns,
+    };
+
+    expect(k9AccessCapabilities).toEqual(k9AccessCapabilities);
+
+    // Unit Test: k9 access capabilities
+    expect(k9AccessCapabilities.allowAdministerResourceArns).toEqual(administerResourceArns);
+    // expect(k9AccessCapabilities.allowAdministerResourceTest).toBe(ArnEqualsTest)
+    expect(k9AccessCapabilities.allowWriteDataArns).toEqual(writeDataArns);
+    expect(k9AccessCapabilities.allowReadDataArns).toEqual(readDataArns);
+    expect(k9AccessCapabilities.allowDeleteDataArns).toEqual(deleteDataArns)
+
+});
+
+test('K9BucketPolicy', () => {
     const app = new cdk.App();
-    // WHEN
+
     const stack = new cdk.Stack(app, 'K9BucketPolicyTest');
     const bucket = new Bucket(stack, 'TestBucket');
-
-    const administerResourceArns = new Set<string>([
-            "arn:aws:iam::139710491120:user/ci",
-        ]
-    );
-
-    const writeDataArns = new Set<string>([
-            "arn:aws:iam::12345678910:role/app-backend",
-        ]
-    );
-    const readDataArns = new Set<string>(writeDataArns)
-        .add("arn:aws:iam::12345678910:role/customer-service");
 
     const k9AccessCapabilities: K9AccessCapabilities = {
         allowAdministerResourceArns: administerResourceArns,
@@ -33,16 +58,8 @@ test('k9 bucket policy', () => {
         k9AccessCapabilities: k9AccessCapabilities
     };
 
-
     // THEN
     expect(k9BucketPolicyProps.k9AccessCapabilities).toEqual(k9AccessCapabilities);
 
-    // Unit Test: k9 access capabilities
-    expect(k9AccessCapabilities.allowAdministerResourceArns).toEqual(administerResourceArns);
-    // expect(k9AccessCapabilities.allowAdministerResourceTest).toBe(ArnEqualsTest)
-    expect(k9AccessCapabilities.allowWriteDataArns).toEqual(writeDataArns);
-    expect(k9AccessCapabilities.allowReadDataArns).toEqual(readDataArns);
-    expect(k9AccessCapabilities.allowDeleteDataArns).toBeUndefined();
-
-    expectCDK(stack).to(haveResource("AWS::S3::Bucket"))
+    expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
 });
