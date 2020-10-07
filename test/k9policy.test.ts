502ee4ab5b4c1670433c50c5fa96d02cc808d2c4
Stephen Kuenzli <skuenzli@k9security.io>
2020-10-07 10:53:04 -0700

    Rename module for k9 policies to k9policy

--- test/k9policy.test.ts
+++ test/k9policy.test.ts
@@ -1,7 +1,7 @@
 import {expect as expectCDK, haveResource, SynthUtils} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-import {K9AccessCapabilities, K9BucketPolicyProps, K9PolicyFactory} from '../lib/k9-cdk-stack';
+import {K9AccessCapabilities, K9BucketPolicyProps, K9PolicyFactory} from '../lib/k9policy';
 
 const administerResourceArns = new Set<string>([
         "arn:aws:iam::139710491120:user/ci",
