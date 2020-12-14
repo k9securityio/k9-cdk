45f80371dd4466e0948e99acd7d9f7a21f1a2302
Stephen Kuenzli <skuenzli@k9security.io>
2020-12-14 11:16:09 -0700

    Generate KMS key policies from the k9 access capability model.

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export import k9policy = require('./k9policy');
+export import kms = require('./kms');
 export import s3 = require('./s3');
---
+++ lib/kms.ts
@@ -0,0 +1,84 @@
+import * as iam from "@aws-cdk/aws-iam";
+import {AccountRootPrincipal, Effect, PolicyDocument, PolicyStatement} from "@aws-cdk/aws-iam";
+import {AccessCapability, AccessSpec, K9PolicyFactory} from "./k9policy";
+import * as cdk from "@aws-cdk/core";
+
+export interface K9KeyPolicyProps {
+    readonly k9DesiredAccess: Array<AccessSpec>
+}
+
+let SUPPORTED_CAPABILITIES = new Array<AccessCapability>(
+    AccessCapability.AdministerResource,
+    AccessCapability.ReadData,
+    AccessCapability.WriteData,
+    AccessCapability.DeleteData,
+);
+
+
+export function makeKeyPolicy(scope: cdk.Construct, id: string, props: K9KeyPolicyProps): PolicyDocument {
+    const policyFactory = new K9PolicyFactory();
+    const policy = new iam.PolicyDocument();
+
+    let resourceArns = [
+        `*`
+    ];
+
+    let allAllowedPrincipalArns = new Set<string>();
+    let wasArnLikeTestUsed = false;
+
+    let accessSpecsByCapability: Map<AccessCapability, AccessSpec> = new Map<AccessCapability, AccessSpec>();
+
+    props.k9DesiredAccess.forEach(accessSpec => accessSpecsByCapability.set(accessSpec.accessCapability, accessSpec));
+
+    for (let supportedCapability of SUPPORTED_CAPABILITIES) {
+        let accessSpec: AccessSpec = accessSpecsByCapability.get(supportedCapability) ||
+            { //generate a default access spec if none was provided
+                accessCapability: supportedCapability,
+                allowPrincipalArns: new Set<string>(),
+                test: "ArnEquals"
+            }
+        ;
+        let arnConditionTest = accessSpec.test || "ArnEquals";
+        if (arnConditionTest == "ArnLike") {
+            wasArnLikeTestUsed = true;
+        }
+
+        let statement = policyFactory.makeAllowStatement(`Restricted-${supportedCapability}`,
+            policyFactory.getActions('KMS', supportedCapability),
+            accessSpec.allowPrincipalArns,
+            arnConditionTest,
+            resourceArns);
+        policy.addStatements(statement);
+
+        accessSpec.allowPrincipalArns.forEach(function (value) {
+            allAllowedPrincipalArns.add(value);
+        });
+    }
+
+    const denyEveryoneElseTest = wasArnLikeTestUsed ? 'ArnNotLike' : 'ArnNotEquals';
+    let denyEveryoneElseStatement = new PolicyStatement({
+        sid: 'DenyEveryoneElse',
+        effect: Effect.DENY,
+        principals: [new AccountRootPrincipal()],
+        actions: ['kms:*'],
+        resources: resourceArns
+    });
+    denyEveryoneElseStatement.addCondition(denyEveryoneElseTest, {
+        'aws:PrincipalArn': [...allAllowedPrincipalArns]
+    });
+
+    policy.addStatements(
+        new PolicyStatement({
+            sid: 'AllowRootUserToAdministerKey',
+            effect: Effect.ALLOW,
+            principals: [new AccountRootPrincipal()],
+            actions: ['kms:*'],
+            resources: resourceArns,
+        }),
+        denyEveryoneElseStatement,
+    );
+
+    policy.validateForResourcePolicy();
+
+    return policy
+}
