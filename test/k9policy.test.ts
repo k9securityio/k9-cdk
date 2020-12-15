ffb6896ab0fc90de31395e2ae918e45fce531b33
Stephen Kuenzli <skuenzli@k9security.io>
2020-12-15 15:58:45 -0700

    Write unit test for K9PolicyFactory#getAllowedPrincipalArns

--- test/k9policy.test.ts
+++ test/k9policy.test.ts
@@ -1,5 +1,5 @@
 import * as k9policy from "../lib/k9policy";
-import {AccessCapability} from "../lib/k9policy";
+import {AccessCapability, AccessSpec} from "../lib/k9policy";
 
 test('K9PolicyFactory#wasLikeUsed', () => {
     let k9PolicyFactory = new k9policy.K9PolicyFactory();
@@ -19,4 +19,24 @@ test('K9PolicyFactory#wasLikeUsed', () => {
             test: "ArnLike"
         }
     ])).toBeTruthy();
+});
+
+test('K9PolicyFactory#getAllowedPrincipalArns', () => {
+    let k9PolicyFactory = new k9policy.K9PolicyFactory();
+    let accessSpecs:Array<AccessSpec> = [
+        {
+            accessCapability: AccessCapability.AdministerResource,
+            allowPrincipalArns: new Set(["arn1", "arn2"]),
+            test: "ArnEquals"
+        },
+        {
+            accessCapability: AccessCapability.ReadData,
+            allowPrincipalArns: new Set(["arn2", "arn3"]),
+            test: "ArnLike"
+        }
+
+    ];
+    expect(k9PolicyFactory.getAllowedPrincipalArns([])).toEqual(new Set<string>());
+    expect(k9PolicyFactory.getAllowedPrincipalArns(accessSpecs))
+        .toEqual(new Set(["arn1", "arn2", "arn3"]));
 });
