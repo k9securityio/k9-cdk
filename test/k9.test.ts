62c713d780a4307e553f6dbb77f52503ea9c62f5
Stephen Kuenzli <skuenzli@k9security.io>
2021-03-02 10:57:39 -0700

    Extract and adopt a stringifyPolicy function

--- test/k9.test.ts
+++ test/k9.test.ts
@@ -1,11 +1,13 @@
 import {expect as expectCDK, haveResource, SynthUtils} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
+import {RemovalPolicy} from '@aws-cdk/core';
 import * as kms from '@aws-cdk/aws-kms';
 import * as s3 from '@aws-cdk/aws-s3';
 import {AccessCapability, AccessSpec} from '../lib/k9policy';
 import {K9BucketPolicyProps} from "../lib/s3";
 import {K9KeyPolicyProps} from "../lib/kms";
 import * as k9 from "../lib";
+import {PolicyDocument} from "@aws-cdk/aws-iam";
 
 // Test the primary public interface to k9 cdk
 
@@ -57,7 +59,7 @@ test('K9BucketPolicy', () => {
     };
     const bucketPolicy = k9.s3.makeBucketPolicy(stack, "S3Bucket", k9BucketPolicyProps);
 
-    console.log("bucketPolicy.document: " + JSON.stringify(bucketPolicy.document.toJSON(), null, 2));
+    console.log("bucketPolicy.document: " + stringifyPolicy(bucketPolicy.document));
 
     expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
     expectCDK(stack).to(haveResource("AWS::S3::BucketPolicy"));
@@ -88,10 +90,15 @@ test('K9KeyPolicy', () => {
     };
     const keyPolicy = k9.kms.makeKeyPolicy(stack, "KMSKey", k9KeyPolicyProps);
 
-    console.log("keyPolicy.document: " + JSON.stringify(keyPolicy.toJSON(), null, 2));
+    console.log("keyPolicy.document: " + stringifyPolicy(keyPolicy));
 
     new kms.Key(stack, 'TestKey', {policy: keyPolicy});
 
     expectCDK(stack).to(haveResource("AWS::KMS::Key"));
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
+
+
+function stringifyPolicy(policyDocument: PolicyDocument) {
+    return JSON.stringify(policyDocument, null, 2);
+}
