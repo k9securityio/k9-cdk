d8cba9a9e4bf1ee013fd94d7133c77f4ad1f7672
Stephen Kuenzli <skuenzli@k9security.io>
2021-09-08 14:29:50 -0700

    Update example app to k9-cdk v0.9.0

--- examples/example.ts
+++ examples/example.ts
@@ -5,21 +5,21 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as k9 from "@k9securityio/k9-cdk";
 import * as kms from "@aws-cdk/aws-kms";
 
-const administerResourceArns = new Set<string>([
-        "arn:aws:iam::12345678910:user/ci",
-        "arn:aws:iam::12345678910:user/person1",
-    ]
-);
+const administerResourceArns = [
+    "arn:aws:iam::12345678910:user/ci",
+    "arn:aws:iam::12345678910:user/person1",
+];
 
-const readConfigArns = new Set<string>(administerResourceArns)
-    .add("arn:aws:iam::12345678910:role/k9-auditor");
+const readConfigArns = administerResourceArns.concat([
+    "arn:aws:iam::12345678910:role/k9-auditor"
+]);
 
-const writeDataArns = new Set<string>([
-        "arn:aws:iam::12345678910:role/app-backend",
-    ]
-);
-const readDataArns = new Set<string>(writeDataArns)
-    .add("arn:aws:iam::12345678910:role/customer-service");
+const writeDataArns = [
+    "arn:aws:iam::12345678910:role/app-backend",
+];
+const readDataArns = writeDataArns.concat([
+    "arn:aws:iam::12345678910:role/customer-service"
+]);
 
 const app = new cdk.App();
 
@@ -30,19 +30,19 @@ const k9BucketPolicyProps: k9.s3.K9BucketPolicyProps = {
     bucket: bucket,
     k9DesiredAccess: new Array<k9.k9policy.AccessSpec>(
         {
-            accessCapability: k9.k9policy.AccessCapability.AdministerResource,
+            accessCapabilities: k9.k9policy.AccessCapability.AdministerResource,
             allowPrincipalArns: administerResourceArns,
         },
         {
-            accessCapability: k9.k9policy.AccessCapability.ReadConfig,
+            accessCapabilities: k9.k9policy.AccessCapability.ReadConfig,
             allowPrincipalArns: readConfigArns,
         },
         {
-            accessCapability: k9.k9policy.AccessCapability.WriteData,
+            accessCapabilities: k9.k9policy.AccessCapability.WriteData,
             allowPrincipalArns: writeDataArns,
         },
         {
-            accessCapability: k9.k9policy.AccessCapability.ReadData,
+            accessCapabilities: k9.k9policy.AccessCapability.ReadData,
             allowPrincipalArns: readDataArns,
         }
         // omit access spec for delete-data because it is unneeded
@@ -57,15 +57,15 @@ writeFileSync('generated.bucket-policy.json',
 const keyPolicyProps: k9.kms.K9KeyPolicyProps = {
     k9DesiredAccess: new Array<k9.k9policy.AccessSpec>(
         {
-            accessCapability: k9.k9policy.AccessCapability.AdministerResource,
+            accessCapabilities: k9.k9policy.AccessCapability.AdministerResource,
             allowPrincipalArns: administerResourceArns,
         },
         {
-            accessCapability: k9.k9policy.AccessCapability.WriteData,
+            accessCapabilities: k9.k9policy.AccessCapability.WriteData,
             allowPrincipalArns: writeDataArns,
         },
         {
-            accessCapability: k9.k9policy.AccessCapability.ReadData,
+            accessCapabilities: k9.k9policy.AccessCapability.ReadData,
             allowPrincipalArns: readDataArns,
         }
         // omit access spec for delete-data because it is unneeded
