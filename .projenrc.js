ab8e66537cc9eb1e8cfb4de3c662914e3216de77
Stephen Kuenzli <skuenzli@k9security.io>
2024-06-17 15:24:14 -0700

    Upgrade project to latest version of CDK and JSII to trigger updates to:
     * @types/node
     * aws-cdk and aws-cdk-lib
     * jsii deps
     * typescript
    
     in order to resolve an issue with eslint processing files with an olde version of typescript.

--- .projenrc.js
+++ .projenrc.js
@@ -15,7 +15,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorOrganization: true,
   authorAddress: 'hello@k9security.io',
 
-  cdkVersion: '2.80.0',
+  cdkVersion: '2.146.0',
+  jsiiVersion: '~5.4.0',
   defaultReleaseBranch: 'v2-main',
   majorVersion: 2,
 
